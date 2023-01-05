d9b08c32dc24d1ff5ceb1d5176e2b615fb474978
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2023-01-05 17:13:56 +0200

    add comments

--- txtfind.c
+++ txtfind.c
@@ -19,6 +19,7 @@ int substring( char * str1, char * str2)
 
 /**
  * check if s is similar to t (s is longer by n)
+ * similar means that s is the same the word except 1 or 0 char
  * 
 */
 int similar (char *s, char *t, int n)
@@ -31,7 +32,7 @@ int similar (char *s, char *t, int n)
     int lens = strlen(s);
     int lent = strlen(t);
 
-    if(lens != lent + n) //check it hey have the same length
+    if(lens != lent + n) //check it they have the same length
     {
         return 0;
     }
@@ -49,20 +50,23 @@ int similar (char *s, char *t, int n)
         ps++;
     }
 
-    if(count == lent)
+    if(count == lent)//check if s contain all the word 
     {
         return 1;
     }
     return 0;
 }
 
+/**
+ * the func change s to be all the line that inserted
+*/
 int getLine(char *s)
 {
     int count = 0;
     char *c = s;
     char ch = '0'; 
     ch = getchar();
-    while (ch != '\n' && count < LINE && ch != EOF)
+    while (ch != '\n' && count < LINE && ch != EOF) //insert all the line - till i get \n , EOF or take full size of it
     {
         c = strncat(c , &ch , 1);
         ch = getchar();
@@ -72,13 +76,16 @@ int getLine(char *s)
     return count;
 }
 
+/**
+ * the func change w to be all the word that inserted
+*/
 int getWord(char *w)
 {
     int count = 0;
     char *c = w;
     char ch = '0'; 
     ch = getchar();
-    while (ch != '\n' && ch != '\t' && ch != ' ' && count < WORD  && ch != EOF)
+    while (ch != '\n' && ch != '\t' && ch != ' ' && count < WORD  && ch != EOF) //insert all the word - till i get \t , ' ' , \n , EOF or take full size of it
     {
         c = strncat(c , &ch , 1);
         ch = getchar();
@@ -88,6 +95,9 @@ int getWord(char *w)
     return count;
 }
 
+/**
+ * the func prints all the lines that contain the word str as substring 
+*/
 void print_lines(char * str) 
 {
     char line[LINE] = {'\0'};
@@ -105,10 +115,13 @@ void print_lines(char * str)
     }
 }
 
+/**
+ * the func prints all the words that contain the word str as or similar to it
+*/
 void print_similar_words(char * str)
 {
     char word[WORD] = {'\0'};
-    for(int i = 0; i<LINE; i++)
+    for(int i = 0; i<LINE; i++) //for the max size of a line
     {
         memset(word , '\0' , WORD);
         if(getWord(word) == 0)
@@ -116,11 +129,11 @@ void print_similar_words(char * str)
             break;
         }
         int sim = 1;
-        if(strlen(word) == strlen(str))
+        if(strlen(word) == strlen(str)) //if they have the same size
         {
             sim = 0;
         }
-        if(similar(word , str , sim) == 1)
+        if(similar(word , str , sim) == 1) //check if similar
         {
             printf("%s\n" ,word);
         }
@@ -129,19 +142,19 @@ void print_similar_words(char * str)
 
 int main()
 {
-    char word[WORD] = {'\0'};
+    char word[WORD] = {'\0'};// get the word
     getWord(word);
     char ch = '0';
-    ch = getchar();
+    ch = getchar(); //get a or b
     char *c = NULL;
     if(ch == 'a')
     {
-        scanf("%s" , c);
+        scanf("%s" , c);//for the \n in the middle
         print_lines(word);
     }
     else if(ch == 'b')
     {
-        scanf("%s" , c);
+        scanf("%s" , c);//for the \n in the middle
         print_similar_words(word);
     }
     return 0;
