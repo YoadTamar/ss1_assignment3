0f4948bd5e66a5d0780085cdc6385b448eae4977
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2023-01-05 17:21:45 +0200

    add comments

--- isort.c
+++ isort.c
@@ -8,16 +8,16 @@ void insertion_sort(int*, int);
 
 int main()
 {
-    int arr[SIZE] = {0}, i = 0, *ptr = &arr[0];
+    int arr[SIZE] = {0}, i = 0, *ptr = &arr; //declearing variables 
 
-    for(i = 0; i < SIZE; i++)
+    for(i = 0; i < SIZE; i++) // get all the numbers
     {
         scanf("%d", (arr + i));
     }
 
-    insertion_sort(arr, SIZE);
+    insertion_sort(arr, SIZE); // sort the array
 
-    for(i = 0; i < SIZE; i++)
+    for(i = 0; i < SIZE; i++) //print the array
     {
         if(i == (SIZE - 1))
         {
@@ -31,49 +31,20 @@ int main()
         ptr += 1;
     }
     printf("\n");
-
-
-    // int arr[] = {0,1,2,3,4,5};
-    // shift_element(arr + 1 , 3);
-    // // 0 x 1 2 3 5
-
-    //1 4 3 5 2  array
-    //1 4 2 5 7  sort 1 
-    //1 4 2 5 7  sort 4
-
-    //1 3 4 5 2  sort 2  (shift l(1 , 1))
-
-    // for (size_t i = 0; i < sizeof(arr)/sizeof(int); i++)
-    // {
-    //     printf("%d " , arr[i]);
-    // }
-    // printf("\n");
-
-    // //1 4 3 5 2  array
-    // int arr[] = {};
-    // for (size_t i = 0; i < sizeof(arr)/sizeof(int); i++)
-    // {
-    //     printf("%d " , arr[i]);
-    // }
-    // printf("\n");
-    // insertion_sort(arr , 0);
-    // for (size_t i = 0; i < sizeof(arr)/sizeof(int); i++)
-    // {
-    //     printf("%d " , arr[i]);
-    // }
-    // printf("\n");
-     
     
     return 0;
 }
 
+/**
+ * move i element in the array from arr pointer right
+*/
 void shift_element(int *arr, int i)
 {   
-    int temp = *(arr + 1);
+    int temp = *(arr + 1); //for the first element
     int *parr = arr + 1;
     *parr = *arr;
 
-    for (size_t j = 1; j < i  ; j++)
+    for (size_t j = 1; j < i  ; j++) //for the rest
     {
         int temp2 = *(arr+j+1);
         parr = arr + j + 1;
@@ -82,12 +53,10 @@ void shift_element(int *arr, int i)
     }
 }
 
-//1 4 3 5 2  array
-//i = 1
-//count = 0
-//cur = 4
-//parr -> arr[0] (=1)
-void insertion_sort(int *arr, int len)
+/**
+ * insertion sort with shift_element
+*/
+void insertion_sort(int *arr, int len) 
 {
     int *parr = NULL;
     for (size_t i = 1; i < len; i++)
